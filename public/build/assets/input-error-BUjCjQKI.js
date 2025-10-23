4e2b6acb4b05f8308702162bb20ea12e6ed72197
khubaib <khubaibfarooq0@gmail.com>
2025-10-23 10:38:22 +0500

    new build with remove delete user

---
+++ public/build/assets/input-error-BUjCjQKI.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-C70V3JXl.js";import{c as o}from"./button-DF8IPEAh.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
